2725ecf2148c214ce465c82ce6cf70f599e2e336
saswat <saswatsingh95747@gmail.com>
2024-10-02 01:31:11 +0530

    new

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -11,6 +11,7 @@ app.post("/hdfcWebhook", async (req, res) => {
         token: string;
         userId: string;
         amount: string
+        
     } = {
         token: req.body.token,
         userId: req.body.user_identifier,
